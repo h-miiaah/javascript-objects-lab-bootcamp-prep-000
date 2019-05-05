f9c516fff6550f1b97ab471533aa5b138c6a82aa
amazing-hashmap-2071 <haim11293@aol.com>
2019-05-05 21:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   delete object.key;
+  return obj;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
   return object;
 }
