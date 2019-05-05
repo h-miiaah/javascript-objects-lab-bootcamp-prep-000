e11ae79697f3a162dcb71350f68661bcfa89f59c
amazing-hashmap-2071 <haim11293@aol.com>
2019-05-05 21:10:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ var obj = {prop: 1};
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
